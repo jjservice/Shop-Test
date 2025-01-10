5179a2efb1c2c1c4e005fff0380d1f153b7d2631
jjservice <jjfabian47@gmail.com>
2025-01-10 17:13:48 -0500

    sugifg

--- public/script.js
+++ public/script.js
@@ -1,13 +1,13 @@
 // Sample product list with images
 const products = [
   { id: 1, name: "Nike Air Force 1'07", price: 1000, image: "./items/NikeAirForce1'07-mainPic.png" }, 
-  { id: 2, name: "Product 2", price: 1500, image: "./razerpic00.jpg" }, 
+  { id: 2, name: "Razer Kishi for iPhone", price: 1500, image: "./razerpic00.jpg" }, 
   { id: 3, name: "Nike Air Force 1'07", price: 1000, image: "./items/NikeAirForce1'07-mainPic.png" },  // Same image as the first product for demonstration purposes only
-  { id: 4, name: "Product 4", price: 1500, image: "./Razerpic5.webp" }, 
+  { id: 4, name: "Razer Kishi for iPhone", price: 1500, image: "./Razerpic5.webp" }, 
   { id: 5, name: "Nike Air Force 1'07", price: 1000, image: "./items/NikeAirForce1'07-mainPic.png" }, 
-  { id: 6, name: "Product 2", price: 1500, image: "./razerpic00.jpg" }, 
+  { id: 6, name: "Razer Kishi for iPhone", price: 1500, image: "./razerpic00.jpg" }, 
   { id: 7, name: "Nike Air Force 1'07", price: 1000, image: "./items/NikeAirForce1'07-mainPic.png" }, 
-  { id: 8, name: "Product", price: 1500, image: "./Razerpic5.webp" } 
+  { id: 8, name: "Razer Kishi for iPhone", price: 1500, image: "./Razerpic5.webp" } 
 ];
 
 // Initialize an empty cart (from localStorage if available)
